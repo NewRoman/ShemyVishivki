081f973ebb9a04dfbb05e49e460e5fd811eea235
romangorbunov <romangorbunov22@gmail.com>
2015-05-17 23:05:52 +0300

    The files structure get ready

---
+++ functions.php
@@ -0,0 +1,36 @@
+// <?php
+//   function footer() {
+//     include 'parts/footer.php';
+//   }
+//   function head() {
+//     include 'parts/header.php';
+//   }
+
+//   function pagination() {
+//     include 'parts/pagination.php';
+//   }
+//   function breadcrumbs() {
+//     include 'parts/breadcrumbs.php';
+//   }
+
+//   function trust_us() {
+//     include 'parts/trust_us.php';
+//   }
+//   function seo_block() {
+//     include 'parts/seo-block.php';
+//   }
+
+//   function what2do_block() {
+//     include 'parts/what2do_block.php';
+//   }
+
+//   function education_institute_main_block() {
+//     include 'parts/education_institute_main_block.php';
+//   }
+
+//   function form_callback() {
+//     include 'parts/form_callback.php';
+//   }
+
+
+
